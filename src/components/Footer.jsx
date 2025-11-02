057860e2229e364a15bebaba791e3294403965dd
Behruz Husanboyev <behruzhusanboyev88@example.com>
2025-11-02 06:36:12 +0500

    yangilandi1

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         <div className="footer-item">
           <h4>Contact Us</h4>
           <p><FaPhone /> +998 (33) 552 36 06</p>
-          <p><FaTelegramPlane /> @yourtelegram</p>
+          <p><FaTelegramPlane /> Telegram</p>
         
           <a href="https://www.instagram.com/chocoberry__namangan/">  <p><FaInstagram />Instagram</p></a>
         </div>
